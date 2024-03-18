e6e96c798e1e6295a63bcc925917780e6a0cdfcc
jun_ <gurwns232@naver.com>
2024-03-18 10:44:05 +0900

    feat: FriendProfile Screen ProfileBody 생성

--- src/screens/FriendProfile.js
+++ src/screens/FriendProfile.js
@@ -3,31 +3,41 @@ import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Ionic from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
+import ProfileBody from '../components/ProfileBody';
 
 const FriendProfile = ({route, navigation}) => {
   const {name, profileImage, post, followers, following} = route.params;
 
   return (
     <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Ionic name="arrow-back" style={{fontSize: 20, color: 'black'}} />
-        </TouchableOpacity>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '92%',
-          }}>
-          <Text style={{fontSize: 15, marginLeft: 10, fontWeight: 'bold'}}>
-            {name}
-          </Text>
-          <Feather
-            name="more-vertical"
-            style={{fontSize: 20, color: 'black'}}
-          />
+      <View style={{padding: 10}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionic name="arrow-back" style={{fontSize: 20, color: 'black'}} />
+          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '92%',
+            }}>
+            <Text style={{fontSize: 15, marginLeft: 10, fontWeight: 'bold'}}>
+              {name}
+            </Text>
+            <Feather
+              name="more-vertical"
+              style={{fontSize: 20, color: 'black'}}
+            />
+          </View>
         </View>
+        <ProfileBody
+          name={name}
+          profileImage={profileImage}
+          post={post}
+          followers={followers}
+          following={following}
+        />
       </View>
     </SafeAreaView>
   );
