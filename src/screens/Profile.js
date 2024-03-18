7b4eb2ee25ca16ff3a653b7af77ce2104dd58e9f
jun_ <gurwns232@naver.com>
2024-03-18 11:31:12 +0900

    feat: Profile Screen UI 생성

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,22 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import ProfileBody from '../components/ProfileBody';
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
+      <View style={{width: '100%', padding: 10}}>
+        <ProfileBody
+          name="jun"
+          accountName="Jun"
+          profileImage={require('../../assets/images/userProfile.jpeg')}
+          followers="300"
+          following="35"
+          post="458"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
