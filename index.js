163e68922789129e3db3480df799bd2ce93ba95f
jun_ <gurwns232@naver.com>
2024-03-19 11:59:37 +0900

    feat: Android Local Push Notification

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
+import PushNotification from 'react-native-push-notification';
 import {name as appName} from './app.json';
+
+PushNotification.configure({
+  onNotification: function (notification) {
+    console.log('notification', notification);
+  },
+
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
 
 AppRegistry.registerComponent(appName, () => App);
