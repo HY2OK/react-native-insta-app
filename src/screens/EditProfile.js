a103b8b0c169e25a9636e51f278f8f77be16b9f5
jun_ <gurwns232@naver.com>
2024-03-19 10:30:54 +0900

    feat: Edit Profile Screen UI 생성

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,11 +1,41 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
 
-const EditProfile = () => {
+const EditProfile = ({route, navigation}) => {
+  const {name, accountName, profileImage} = route.params;
+
   return (
-    <View>
-      <Text>EditProfile</Text>
-    </View>
+    <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: 10,
+        }}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text>취소</Text>
+        </TouchableOpacity>
+
+        <Text style={{fontSize: 16, fontWeight: 'bold'}}>프로필 수정</Text>
+
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={{color: '#3493D9'}}>완료</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{padding: 20, alignItems: 'center'}}>
+        <Image
+          source={profileImage}
+          style={{width: 80, height: 80, borderRadius: 100}}
+        />
+        <Text style={{color: '#3493D9', marginTop: 10}}>
+          프로필 사진 바꾸기
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
